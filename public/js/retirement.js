796f0ccc06d51c7271f73dce71eecbb5e10e4b94
GaohanC <gaohanc@alumni.ubc.ca>
2017-07-14 14:25:07 -0700

    change for today

--- public/js/retirement.js
+++ public/js/retirement.js
@@ -15,8 +15,8 @@ $( document ).ready(function() {
 });
 function calculate_saving(q1, q2, q3, q4, q5, q6, q7) {
     if (q1=="" || isNaN(q1)) q1=10000;
-    if (q2=="" || isNaN(q2)) q2=900*12;
-    if (q3=="" || isNaN(q3)) q3=400*12;
+    if (q2=="" || isNaN(q2)) q2=6942.36;
+    if (q3=="" || isNaN(q3)) q3=8221.32;
     if (q4=="" || isNaN(q4)) q4=1;
     if (q5=="" || isNaN(q5)) q5=0;
     if (q6=="" || isNaN(q6)) q6=0;
@@ -26,9 +26,14 @@ function calculate_saving(q1, q2, q3, q4, q5, q6, q7) {
     denominator=q4;
     answer = numerator1*numerator2/denominator-q6-q7;
     if(answer>0){
-        output=("恭喜您，您有相对充裕的退休储蓄，妥善计划您的存款可以让您的退休生活更美好!");
+        output=("我们预估为$"+toInteger(answer)+". 恭喜您，您有相对充裕的退休储蓄，妥善计划您的存款可以让您的退休生活更美好!");
     }else{
-        output=("您的退休存款可能会面临不足，了解我们的储蓄计划可以帮您更好的规划您的退休存款")
+        output=("我们预估为$"+toInteger(answer)+". 您的退休存款可能会面临不足，了解我们的储蓄计划可以帮您更好的规划您的退休存款")
     }
     return output;
 }
+function toInteger(number){ 
+  return Math.round(  
+    Number(number)    
+  ); 
+};
